55fc0d5a28a5c85a8d98ccd5fe823d00cc16c7c3
lorenestauffer <lorene.stauffer@campus-centre.fr>
2025-01-30 23:13:44 +0100

    ajout put et delete mais ne fonctionne pas

--- index.js
+++ index.js
@@ -17,8 +17,57 @@ let produits = [
 // Liste des produits
 app.get("/produits", (req,res) => {
     res.json(produits);
-
 })
+
+
+// POST
+
+app.post("/produits", (req, res) => {
+
+    const newProduit = req.body;
+    if (!newProduit || !newProduit.id || !newProduit.nom) { // Vérification des champs
+        return res.status(400).json({ message: 'Invalid produits data' });
+    }
+
+    produits.push(newProduit); //
+    res.status(201).json(newProduit);
+});
+
+
+
+// PUT
+
+app.put("/produits/:id", (req, res) => {
+    const id = req.params;
+    const produitId = parseInt(req.params.id);
+    let produit = produits.find(produit => produit.id === produitId);
+
+    if (!produit) {
+        return res.status(404).json({ message: 'Produit non trouvé' });
+    }
+
+    res.status(200).json(produit);
+});
+
+
+
+// DELETE
+
+
+app.delete('/produits/:id', (req, res) => {
+    const produitId = parseInt(req.params.id);  // Récupère et convertit l'ID en entier
+    const produitIndex = produits.findIndex(produit => produit.id === produitId);  // Recherche l'index du produit
+
+    if (produitIndex === -1) {
+        return res.status(404).json({ message: 'Produit non trouvé' });  // Si le produit n'existe pas affiche ce message
+    }
+
+    produits.splice(produitIndex, 1);  // Supprime le produit à l'index trouvé
+    res.status(200).json(produits);  // Renvoie la liste mise à jour des produits
+});
+
+
+
 
 
 // Démarrage du serveur
